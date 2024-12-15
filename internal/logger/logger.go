38c0d99352e22900a0a0118be4fd94a362ab2ae2
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-12-15 12:06:12 +0200

    add air

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,11 @@
 package logger
 
 import (
+	"os"
+	"path"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"ypeskov/qr-generator/internal/config"
 
 	log "github.com/sirupsen/logrus"
@@ -17,15 +21,37 @@ func (hook ContextHook) Levels() []log.Level {
 	return log.AllLevels
 }
 
-func (hook ContextHook) Fire(entry *log.Entry) error {
-	if pc, _, line, ok := runtime.Caller(8); ok {
-		funcName := runtime.FuncForPC(pc).Name()
-		entry.Data["func"] = funcName
-		//entry.Data["func"] = path.Base(funcName)
-		entry.Data["line"] = line
-	}
+var projectRoot string
 
+func init() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic("unable to determine working directory: " + err.Error())
+	}
+	projectRoot = filepath.ToSlash(cwd) + "/"
+}
+
+func (hook ContextHook) Fire(entry *log.Entry) error {
+	for i := 4; i < 10; i++ {
+		if pc, file, line, ok := runtime.Caller(i); ok {
+			funcName := runtime.FuncForPC(pc).Name()
+			funcName = path.Base(funcName)
+
+			file = strings.TrimPrefix(file, projectRoot)
+
+			if !isLogrusFile(file) {
+				entry.Data["func"] = funcName
+				entry.Data["file"] = file
+				entry.Data["line"] = line
+				break
+			}
+		}
+	}
 	return nil
+}
+
+func isLogrusFile(file string) bool {
+	return strings.Contains(file, "sirupsen/logrus")
 }
 
 func New(cfg *config.Config) *Logger {
@@ -37,6 +63,7 @@ func New(cfg *config.Config) *Logger {
 		level = log.InfoLevel
 	}
 	l.SetLevel(level)
+	log.SetReportCaller(true)
 	l.AddHook(ContextHook{})
 
 	return &Logger{l}
